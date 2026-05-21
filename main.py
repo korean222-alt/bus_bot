cce0854bca5f1216ace47c385d875532b58be42a
korean222-alt <gktgkt2309@gmail.com>
2026-05-21 18:31:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, fil
 logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ALLOWED_USERS = [123456789, 987654321]
+ALLOWED_USERS = [8058101860]
 
 KST = pytz.timezone('Asia/Seoul')
 
